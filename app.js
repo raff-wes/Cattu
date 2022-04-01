a0f37730b88b0646bd9d7252acc7c94721ec1d88
raff-wes <raf.wes.dev@gmail.com>
2022-04-01 13:23:10 +0200

    Major files update

--- app.js
+++ app.js
@@ -1,79 +1,94 @@
 var express = require('express');
 var http = require('http');
 var path = require("path");
-//var bodyParser = require('body-parser');
 var helmet = require('helmet');
 var rateLimit = require("express-rate-limit");
-// var mongoose = require("mongoose");
-// mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://rafal:rafal@cluster0.gsf4h.mongodb.net/test');
-// const express = require('express');
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const { body, validationResult } = require('express-validator');
 const { title } = require('process');
 
-
 const app = express(),
     { engine } = require('express-handlebars'),
     bodyParser = require('body-parser');
 
-
 app.set('view engine', 'hbs');
-
 
 app.use(express.static("public"));
 
-app.use(express.json({ extended: false })); //This is the line that you want to add
+app.use(express.json({ extended: false }));
 
-app.use(bodyParser({ extended: false }))
+//app.use(bodyParser({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('handlebars', engine({
     defaultLayout: 'main'
 }))
 app.set('view engine', 'handlebars')
 
-app.post('/added', function(sReq, sRes) {
-    console.log('title:', sReq.body.title);
-    //var newTitle = sReq.body.title;
-    console.log('author:', sReq.body.author);
-    console.log('mood:', sReq.body.mood);
-    console.log('time:', sReq.body.time);
-    console.log('date:', sReq.body.date);
+async function readingFromForm() {
+    console.log('data download has started');
+    console.log('3');
+    app.post('/added', function(sReq, sRes) {
+        var newTitle = sReq.body.title;
+        console.log('title:', newTitle);
 
-    sRes.sendStatus(200);
-});
+        var newAuthor = sReq.body.author;
+        console.log('author:', newAuthor);
 
+        var newMood = sReq.body.mood;
+        console.log('mood:', newMood);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+        var newTime = sReq.body.time;
+        console.log('time:', newTime);
+
+        var newDate = sReq.body.date;
+        console.log('date:', newDate);
+
+        sRes.sendStatus(200);
+
+        console.log(sReq); //Caution! It generates a lot of "spam" in the console !!!
+        console.log(sRes); //Caution! It generates a lot of "spam" in the console !!!
+
+    });
+    console.log('4');
+    console.log('data has been downloaded');
+}
 
 async function main() {
 
-
-    const uri = "mongodb+srv://rafal:rafal@cluster0.gsf4h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
+    const uri = "mongodb+srv://rafal:rafal@cluster0.gsf4h.mongodb.net/cattu?retryWrites=true&w=majority";
 
     const client = new MongoClient(uri);
 
     try {
+        console.log('START');
+        console.log('1');
         await client.connect(); // Connect to the MongoDB cluster
 
+        console.log('2');
+        await readingFromForm();
+
+        console.log('5');
         await createListing(client, {
-            title: "cokolwiek kurde",
-            author: "Twenty One Pilots",
-            mood: "Nostalgic",
-            time: "Evening",
-            date: "2022-09-09T23:00:00.000+00:00"
+            title: "newTitle",
+            author: "newAuthor",
+            mood: "newMood",
+            time: "newTime",
+            date: "newDate" // YYYY-MM-DD
         })
+        console.log('END');
 
     } catch (e) {
         console.error(e);
     } finally {
+
         await client.close();
     }
 }
 main().catch(console.error);
 async function createListing(client, newListing) {
+
     const result = await client.db("cattu").collection("test1").insertOne(newListing);
     console.log(`New listing created with the following id: ${result.insertedId}`);
 }
